5bba61f91f33a0bfebc3c1be8a1fba7c86a15614
Joel Kelley <jkelley@georgefox.edu>
2015-10-01 20:18:06 -0600

    Should use mysqldb on aws now

--- picture_hunt/config.py
+++ picture_hunt/config.py
@@ -1,11 +1,16 @@
 import os
+from picture_hunt.secrets import DB_USER, DB_PASSWD
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 UPLOADS = os.path.join(basedir, 'static/tmp')
 S3_BUCKET = 'picture-hunt'
 
-SQLALCHEMY_DATABASE_PATH = os.path.join(basedir, '../pic.db')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + SQLALCHEMY_DATABASE_PATH
+
+if socket.gethostname() == "aws":
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{passwd}@localhost/pic'.format(user=DB_USER, passwd=DB_PASSWD)
+else:
+    SQLALCHEMY_DATABASE_PATH = os.path.join(basedir, '../pic.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + SQLALCHEMY_DATABASE_PATH
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
